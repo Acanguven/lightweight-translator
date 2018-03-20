21eb281a6a33f2117563ed28dd133d870ae25a1e
Ahmetcan <ahmetcan.guven@trendyol.com>
2018-03-20 14:53:39 +0300

    Default language

--- index.js
+++ index.js
@@ -1,97 +1,53 @@
-class Translator {
-  constructor() {
-    this.languages = {};
-
-    this.__cache = {};
-    this.defaultLanguage = null;
-  }
-
-  /**
-   * Sets the default language
-   * @param languageKey
-   */
-  setDefault(languageKey){
-    this.defaultLanguage = languageKey;
-  }
-
-  /**
-   * Translates the given string
-   * @param name
-   * @param language
-   * @param variableSet
-   * @returns {*}
-   */
-  translate(name, language, variableSet) {
-    if(!language && this.defaultLanguage) {
-      language = this.defaultLanguage;
-    }
-    const hash = this.__createHash.apply(null, arguments);
-    if (this.__cache[hash]) return this.__cache[hash];
-
-    let translation = null;
-    if (this.languages[language]) translation = this.languages[language][name];
-    if (!translation) return this.__bundleUnresolvedTranslation.apply(null, arguments);
-
-    if(variableSet) {
-      translation = this.__buildVariables(translation, language, variableSet)
-    }
-
-    this.__cache[hash] = translation;
-    return translation;
-  }
-
-  /**
-   * Adds new language to the library
-   * @param languageKey
-   * @param dictionary
-   */
-  addLanguage(languageKey, dictionary) {
-    this.languages[languageKey] = dictionary;
-  }
-
-  /**
-   * Returns hash for memoizing
-   * @param name
-   * @param language
-   * @param variableSet
-   * @returns {string}
-   * @private
-   */
-  __createHash(name, language, variableSet) {
-    return name + language + (variableSet ? JSON.stringify(variableSet) : '');
-  }
-
-
-  /**
-   * Creates text for untranslated value
-   * @param name
-   * @param language
-   * @param variableSet
-   * @returns {string}
-   * @private
-   */
-  __bundleUnresolvedTranslation(name, language, variableSet) {
-    return '*' + name + '|' + language + '|' + (variableSet ? JSON.stringify(variableSet) : '') + '*';
-  }
-
-  /**
-   * Replaces variables with language values
-   * @param replaceString
-   * @param language
-   * @param variables
-   * @returns {*}
-   * @private
-   */
-  __buildVariables(replaceString, language, variables) {
-    for (let prop in variables) {
-      let value = null;
-      if (this.languages[language]) {
-        value = this.languages[language][variables[prop]];
-      }
-      replaceString = replaceString.replace(new RegExp('{' + prop + '}', "g"), value || variables[prop]);
-    }
-    return replaceString;
-  }
+function Translator(){
+  this.languages = {};
+  this.__cache = {};
+  this.defaultLanguage = null;
 }
 
-module.exports = new Translator();
+Translator.prototype.setDefault = function(languageKey){
+  this.defaultLanguage = languageKey;
+}
+
+Translator.prototype.translate = function(name, language, variableSet){
+  if(!language && this.defaultLanguage) {
+    language = this.defaultLanguage;
+  }
+  const hash = this.__createHash.apply(null, arguments);
+  if (this.__cache[hash]) return this.__cache[hash];
+
+  let translation = null;
+  if (this.languages[language]) translation = this.languages[language][name];
+  if (!translation) return this.__bundleUnresolvedTranslation.apply(null, arguments);
+
+  if(variableSet) {
+    translation = this.__buildVariables(translation, language, variableSet)
+  }
+
+  this.__cache[hash] = translation;
+  return translation;
+}
+
+Translator.prototype.addLanguage = function(languageKey, dictionary){
+  this.languages[languageKey] = dictionary;
+}
+
+Translator.prototype.__createHash = function(name, language, variableSet) {
+  return name + language + (variableSet ? JSON.stringify(variableSet) : '');
+}
+
+Translator.prototype.__bundleUnresolvedTranslation = function (name, language, variableSet) {
+  return '*' + name + '|' + language + '|' + (variableSet ? JSON.stringify(variableSet) : '') + '*';
+}
+
+Translator.prototype.__buildVariables = function(replaceString, language, variables) {
+  for (let prop in variables) {
+    let value = null;
+    if (this.languages[language]) {
+      value = this.languages[language][variables[prop]];
+    }
+    replaceString = replaceString.replace(new RegExp('{' + prop + '}', "g"), value || variables[prop]);
+  }
+  return replaceString;
+}
+
+module.exports = Translator;
--- test/translator.spec.js
+++ test/translator.spec.js
@@ -1,122 +1,124 @@
 const expect = require('chai').expect;
 const Translator = require('../');
 
+const translator = new Translator();
+
 describe('Translator', function () {
   beforeEach(() => {
-    Translator.languages = {};
-    Translator.__cache = {};
+    translator.languages = {};
+    translator.__cache = {};
   });
 
   it('should expose module', function () {
-    expect(Translator).to.be.an('object');
+    expect(Translator).to.be.an('function');
   });
 
   it('should expose defined languages object', function () {
-    expect(Translator.languages).to.be.an('object');
+    expect(translator.languages).to.be.an('object');
   });
 
   it('should extend languages with provided object', function () {
-    Translator.addLanguage('TR', {
+    translator.addLanguage('TR', {
       book: 'kitap'
     });
 
-    expect(Translator.languages.TR).to.deep.eq({
+    expect(translator.languages.TR).to.deep.eq({
       book: 'kitap'
     });
   });
 
   it('should cache translation', function () {
-    const hash = Translator.__createHash('test', 'TR', {test: 'test'});
+    const hash = translator.__createHash('test', 'TR', {test: 'test'});
 
     expect(hash).to.eq('testTR{"test":"test"}');
   });
 
 
   it('should print untranslated text for dictionary value', function () {
-    const unResolved = Translator.__bundleUnresolvedTranslation('test', 'TR', {value: 5});
+    const unResolved = translator.__bundleUnresolvedTranslation('test', 'TR', {value: 5});
 
     expect(unResolved).to.eq('*test|TR|{"value":5}*');
   });
 
   it('should replace variables in translation string', function () {
-    Translator.addLanguage('TR', {});
-    const translated = Translator.__buildVariables('I just finished {book}', 'TR', {book: 'Sherlock Holmes'});
+    translator.addLanguage('TR', {});
+    const translated = translator.__buildVariables('I just finished {book}', 'TR', {book: 'Sherlock Holmes'});
 
     expect(translated).to.eq('I just finished Sherlock Holmes');
   });
 
   it('should replace variables with deep translation', function () {
-    Translator.addLanguage('TR', {
+    translator.addLanguage('TR', {
       book_lotr: 'Yüzüklerin Efendisi'
     });
 
-    Translator.addLanguage('EN', {
+    translator.addLanguage('EN', {
       book_lotr: 'Lord Of The Rings'
     });
 
-    const translated_tr = Translator.__buildVariables('I just finished {book}', 'TR', {book: 'book_lotr'});
-    const translated_en = Translator.__buildVariables('I just finished {book}', 'EN', {book: 'book_lotr'});
+    const translated_tr = translator.__buildVariables('I just finished {book}', 'TR', {book: 'book_lotr'});
+    const translated_en = translator.__buildVariables('I just finished {book}', 'EN', {book: 'book_lotr'});
 
     expect(translated_en).to.eq('I just finished Lord Of The Rings');
     expect(translated_tr).to.eq('I just finished Yüzüklerin Efendisi');
   });
 
   it('should translate given string', function () {
-    Translator.addLanguage('TR', {
+    translator.addLanguage('TR', {
       status_available: 'Durum: Uygun'
     });
 
-    const translated_tr = Translator.translate('status_available', 'TR');
+    const translated_tr = translator.translate('status_available', 'TR');
     expect(translated_tr).to.eq('Durum: Uygun');
   });
 
   it('should print unresolved for not added language', function () {
-    const translated_tr = Translator.translate('status_available', 'TR');
+    const translated_tr = translator.translate('status_available', 'TR');
     expect(translated_tr).to.eq('*status_available|TR|*');
   });
 
   it('should translate with variables', function () {
-    Translator.addLanguage('TR', {
+    translator.addLanguage('TR', {
       status_available: 'Durum: {status}'
     });
 
-    const translated_tr = Translator.translate('status_available', 'TR', {status: 'Uygun'});
+    const translated_tr = translator.translate('status_available', 'TR', {status: 'Uygun'});
     expect(translated_tr).to.eq('Durum: Uygun');
   });
 
   it('should translate with deep translation', function () {
-    Translator.addLanguage('TR', {
+    translator.addLanguage('TR', {
       status: 'Durum: {status}',
       available: 'Uygun'
     });
 
-    const translated_tr = Translator.translate('status', 'TR', {status: 'available'});
+    const translated_tr = translator.translate('status', 'TR', {status: 'available'});
     expect(translated_tr).to.eq('Durum: Uygun');
   });
 
   it('should respond from cache if same translation requested', function () {
-    Translator.addLanguage('TR', {
+    translator.addLanguage('TR', {
       status: 'Durum: {status}',
       available: 'Uygun'
     });
 
-    const translated = Translator.translate('status', 'TR', {status: 'available'});
+    const translated = translator.translate('status', 'TR', {status: 'available'});
     expect(translated).to.eq('Durum: Uygun');
 
-    Translator.__cache['statusTR{"status":"available"}'] = 'changed memory';
+    translator.__cache['statusTR{"status":"available"}'] = 'changed memory';
 
-    const translated_from_cache = Translator.translate('status', 'TR', {status: 'available'});
+    const translated_from_cache = translator.translate('status', 'TR', {status: 'available'});
     expect(translated_from_cache).to.eq('changed memory');
   });
 
   it('should set default language', function () {
-    Translator.addLanguage('TR', {
+    translator.addLanguage('TR', {
       status_available: 'Durum: Uygun'
     });
 
-    Translator.setDefault('TR');
+    translator.setDefault('TR');
 
-    const translated_tr = Translator.translate('status_available');
+    const translated_tr = translator.translate('status_available');
     expect(translated_tr).to.eq('Durum: Uygun');
   });
 });
